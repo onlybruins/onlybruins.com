91d22e0880caa3f7bd27eb4a9d0cff73c1797ed9
Tom Binford <tombinford@ucla.edu>
2023-04-25 12:37:58 -0700

    Move API to an express Router, create global build and start scripts

---
+++ backend/src/api.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import { faker } from '@faker-js/faker';
+
+const api = express.Router();
+
+class FakePost {
+    username: string;
+    postDate: Date;
+    imageUrl: string;
+    tippedAmount?: number;
+}
+
+function createRandomFakePost(): FakePost {
+    const username = faker.internet.userName();
+    const postDate = faker.date.between('2015-01-01T00:00:00.000Z', '2023-05-01T00:00:00.000Z');
+    const imageUrl = faker.image.fashion();
+    const tippedAmount = faker.helpers.arrayElement([undefined, faker.datatype.number(100)])
+
+    return {
+        username,
+        postDate,
+        imageUrl,
+        tippedAmount
+    };
+}
+
+api.get('/fakePosts', (req, res) => {
+    const data = Array.apply(null, Array(20)).map(() => createRandomFakePost());
+    res.json(data);
+});
+
+export default api;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,15 @@
 import express from 'express'
-import { faker } from '@faker-js/faker';
+import apiRouter from './api';
 
 const app = express()
 const port = 3070
 
-class FakePost {
-  username: string;
-  postDate: Date;
-  imageUrl: string;
-  tippedAmount?: number;
-}
-
-function createRandomFakePost(): FakePost {
-  const username = faker.internet.userName();
-  const postDate = faker.date.between('2015-01-01T00:00:00.000Z', '2023-05-01T00:00:00.000Z');
-  const imageUrl = faker.image.fashion();
-  const tippedAmount = faker.helpers.arrayElement([undefined, faker.datatype.number(100)])
-
-  return {
-    username,
-    postDate,
-    imageUrl,
-    tippedAmount
-  };
-}
-
-const post = createRandomFakePost();
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
-app.get('/api/fakePosts', (req, res) => {
-  const data = Array.apply(null, Array(20)).map(() => createRandomFakePost());
-  res.json(data)
-})
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`OnlyBruins API listening on port ${port}`)
+});
