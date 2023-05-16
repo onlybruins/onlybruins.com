26a0bfda1e2f04bd4ec4266e336bb2349f59a4fc
Michael Lan <michaellan202@gmail.com>
2023-05-16 12:47:28 -0700

    add https

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import apiRouter from './api';
 import path from 'path';
+import https from 'https';
+import http from 'http';
+var fs = require('fs');
 
 const app = express();
-const port = process.env.NODE_ENV === 'production' ? 80 : 8080;
+
+var privateKey = fs.readFileSync('../../privatekey.key', 'utf8');
+var certificate = fs.readFileSync('../../certificate.pem', 'utf8');
+
+var credentials = { key: privateKey, cert: certificate };
 
 app.use(express.static(path.resolve(__dirname, '../../frontend/build')));
 
@@ -13,6 +20,13 @@ app.get('/', (req, res) => {
 
 app.use('/api', apiRouter);
 
-app.listen(port, () => {
-  console.log(`OnlyBruins API listening on port ${port}`)
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080, () => {
+  console.log(`OnlyBruins API listening on port ${8080}`)
+});
+
+httpsServer.listen(8443, () => {
+  console.log(`OnlyBruins API listening on port ${8443}`)
 });
