1faf67438f10918be73f2db8bad8721453d0d9d3
Michael Lan <michaellan202@gmail.com>
2023-05-16 14:47:16 -0700

    formatting

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -4,29 +4,29 @@ import { faker } from '@faker-js/faker';
 const api = express.Router();
 
 class FakePost {
-    username: string;
-    postDate: Date;
-    imageUrl: string;
-    tippedAmount?: number;
+  username: string;
+  postDate: Date;
+  imageUrl: string;
+  tippedAmount?: number;
 }
 
 function createRandomFakePost(): FakePost {
-    const username = faker.internet.userName();
-    const postDate = faker.date.between('2015-01-01T00:00:00.000Z', '2023-05-01T00:00:00.000Z');
-    const imageUrl = faker.image.fashion(1280, 720, true);
-    const tippedAmount = faker.helpers.arrayElement([undefined, faker.datatype.number(100)])
+  const username = faker.internet.userName();
+  const postDate = faker.date.between('2015-01-01T00:00:00.000Z', '2023-05-01T00:00:00.000Z');
+  const imageUrl = faker.image.fashion(1280, 720, true);
+  const tippedAmount = faker.helpers.arrayElement([undefined, faker.datatype.number(100)])
 
-    return {
-        username,
-        postDate,
-        imageUrl,
-        tippedAmount
-    };
+  return {
+    username,
+    postDate,
+    imageUrl,
+    tippedAmount
+  };
 }
 
-api.get('/fakePosts', (req, res) => {
-    const data = Array.apply(null, Array(20)).map(() => createRandomFakePost());
-    res.json(data);
+api.get('/fakePosts', (_req, res) => {
+  const data = Array.apply(null, Array(20)).map(() => createRandomFakePost());
+  res.json(data);
 });
 
 export default api;
