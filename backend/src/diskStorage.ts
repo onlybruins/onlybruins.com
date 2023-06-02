0f9d35a5c22e5cea186e832130ba7e6a2749edea
Tom Binford <tombinford@ucla.edu>
2023-06-01 18:47:33 -0700

    fix: make directories for multer to put images

--- backend/src/diskStorage.ts
+++ backend/src/diskStorage.ts
@@ -7,13 +7,10 @@ export type RequestWithUUID = Request & { image_uuid: string };
 
 export const UgcStorage = multer.diskStorage({
     destination: (req: RequestWithUUID, file, callback) => {
-        // TODO: create the two-letter directory if it doesn't exist. multer won't do it for us.
-        console.log('UgcStorage:destination: returning', path.resolve(__dirname, '../../ugc-images', req.image_uuid.slice(0, 2)));
         callback(null, path.resolve(__dirname, '../../ugc-images', req.image_uuid.slice(0, 2)));
     },
     filename: (req: RequestWithUUID, file, callback) => {
         // TODO: add file extension based on file.mimetype
-        console.log('UgcStorage:filename: returning', req.image_uuid.slice(2) + '.jpg');
         callback(null, req.image_uuid.slice(2) + '.jpg');
     }
 });
