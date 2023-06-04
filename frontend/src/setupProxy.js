9d83b799edd04615cfe011e273d65362d4d590b8
Tom Binford <tombinford@ucla.edu>
2023-06-03 19:10:27 -0700

    tooling: proxy /images in addition to /api in npm run dev

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    ['/api', '/images'],
     createProxyMiddleware({
       target: 'http://localhost:8080',
       changeOrigin: true,
