fff15ef430f80d89bdcdabecd7bb1b95537f4e1f
Michael Lan <michaellan202@gmail.com>
2023-06-05 10:28:15 -0700

    move post to new file

--- frontend/src/Post.tsx
+++ frontend/src/Post.tsx
@@ -9,25 +9,33 @@ import {
   InputLeftElement,
   InputRightElement,
   InputGroup,
-  CheckboxIcon,
-  Center,
+  Icon,
   Button,
-  useColorModeValue,
 } from "@chakra-ui/react"
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import { Coins, CurrencyCircleDollar } from "phosphor-react"
+import { useState } from "react";
 
 interface PostProps {
-  username: string,
-  postDate: string,
   imageUrl: string,
-  tippedAmount?: number,
+  postDate: string,
+  username: string,
 }
 
-export const Post = ({
-  username, postDate, imageUrl, tippedAmount
-}: PostProps) => {
-  const coinColor = useColorModeValue("#c4aa7e", "green");
+const Post = ({ imageUrl, postDate, username }: PostProps) => {
+  const [tippedAmount, setTippedAmount] = useState('');
+  const [submit, setSubmit] = useState(false);
+
+  const handleChange = (event: any) => {
+    setTippedAmount(event.target.value);
+  }
+
+  const handleSubmit = () => {
+    if (!isNaN(Number(tippedAmount)) && (Number(tippedAmount) > 0)) {
+      setSubmit(!submit);
+    }
+  }
+
   return (
     <Card>
       <CardHeader paddingBottom="0px">
@@ -41,28 +49,31 @@ export const Post = ({
         <Image borderRadius="8px" boxSize="100%" src={imageUrl} mb="10px" />
         <InputGroup>
           <InputLeftElement
+            pointerEvents='none'
             color='gray.300'
             fontSize='1.2em'
-          >
-          {
-            tippedAmount ?
+            children='$'
+          />
+          <Input
+            value={tippedAmount}
+            onChange={handleChange}
+            placeholder='How much would you like to tip?' />
+          <InputRightElement width='4.5rem'>
+            {submit ?
               <Tooltip label={`You've tipped ${tippedAmount} bruinbux`} fontSize='md'>
-                <Coins color={coinColor} weight="fill" size={24} />
+                <Icon color="#c4aa7e" />
               </Tooltip>
               :
-              <Tooltip label={`Tip bruinbux to ${username}!`} fontSize='md'>
-                <Coins color={coinColor} weight="regular" size={24} />
-              </Tooltip>
-          }
-          </InputLeftElement>
-          <Input placeholder='How much would you like to tip?' />
-          <InputRightElement>
-            <CheckboxIcon color='green.500' />
+              <Button h='1.75rem' size='sm'
+                onClick={handleSubmit}>
+                Tip
+              </Button>
+            }
           </InputRightElement>
         </InputGroup>
-        <HStack paddingTop="1rem">
-        </HStack>
       </CardBody>
-    </Card >
+    </Card>
   )
 }
+
+export default Post;
