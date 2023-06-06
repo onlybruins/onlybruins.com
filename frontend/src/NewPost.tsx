b5de60c708f3996711dae807f0d6ceeab59ff673
Michael Lan <michaellan202@gmail.com>
2023-06-06 12:50:57 -0700

    feat(UI): add toasts for new posts

--- frontend/src/NewPost.tsx
+++ frontend/src/NewPost.tsx
@@ -1,44 +1,59 @@
-import { Button, Card } from "@chakra-ui/react";
+import { Button, Card, useToast } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
+import useAppStore from "./appStore";
 import FileUpload from "./FileUpload";
 
-const username = 'T Omegalul M';
+export const NewPost = () => {
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<{ postImage: File }>();
 
-const onSubmit = (data: {postImage: File}) => {
+  const username = useAppStore((state) => state.username!);
+
+  const onSubmit = (data: { postImage: File }, callback: ((a: 'success' | 'error') => void)) => {
     const formData = new FormData();
     formData.append('postImage', data.postImage);
     fetch(encodeURI(`/api/users/${username}/posts`), {
       method: "POST",
       body: formData
     })
-    .then(res => res.json())
-    .then(json => console.log('response:', json))
-    .catch(console.error);
-}
-
-export const NewPost = () => {
-    const {
-      handleSubmit,
-      register,
-      setError,
-      control,
-      formState: { errors, isSubmitting },
-    } = useForm<{postImage: File}>();
-    return (
-      <Card width='100%'>
-        <FileUpload name="postImage"
-          acceptedFileTypes="image/*"
-          isRequired={true}
-          placeholder="Upload an image..."
-          control={control}>
-          Make a new post!
-        </FileUpload >
-        <Button type="submit"
-          onClick={handleSubmit(onSubmit)}>
-          Post
-        </Button>
-      </Card>
-    )
+      .then(res => res.json())
+      .then(json => {
+        console.log('response:', json)
+        callback('success')
+      })
+      .catch((e) => {
+        console.error(e)
+        callback('error')
+      }).finally(() => reset());
   }
+
+  const toast = useToast();
+  return (
+    <Card width='100%'>
+      <FileUpload name="postImage"
+        acceptedFileTypes="image/*"
+        isRequired={true}
+        placeholder="Upload an image..."
+        control={control}>
+        Make a new post!
+      </FileUpload >
+      <Button type="submit"
+        isLoading={isSubmitting}
+        onClick={handleSubmit((data) => onSubmit(data, (status) => {
+          toast({
+            status: status,
+            position: 'bottom-right',
+            title: status === 'success' ? "posted successfully!" : "could not post :(",
+          })
+        }))}>
+        Post
+      </Button>
+    </Card>
+  )
+}
 
 export default NewPost;
