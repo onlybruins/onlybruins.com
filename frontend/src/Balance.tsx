8c81877413dfcb648d50c48aa1bee008abf43719
Michael Lan <michaellan202@gmail.com>
2023-06-08 08:25:09 -0700

    feat: use countUp for nice balance transitions

--- frontend/src/Balance.tsx
+++ frontend/src/Balance.tsx
@@ -2,8 +2,17 @@ import { Text, Box, Center, HStack, Spinner, useColorModeValue, useToken } from 
 import { useQuery } from "@tanstack/react-query";
 import { Coins } from "phosphor-react";
 import useAppStore from "./appStore";
+import { useCountUp } from 'react-countup';
+import { useRef } from "react";
 
 export const Balance = () => {
+  const countUpRef = useRef(null);
+  const { update } = useCountUp({
+    ref: countUpRef,
+    duration: 3,
+    start: 0,
+    end: 0,
+  });
   const boxColor = useColorModeValue('yellow.400', 'yellow.500');
   const iconColor = useColorModeValue('yellow.600', 'yellow.700');
   const [iconRgb] = useToken('colors', [iconColor]);
@@ -11,12 +20,12 @@ export const Balance = () => {
   const { data, isLoading } = useQuery({
     queryKey: ['balance'],
     queryFn: async () => {
-      console.log('fetching balance...')
       const resp = await fetch(`/api/users/${username}/balance`)
       if (!resp.ok) {
         throw new Error('error occurred while getting balance')
       }
       const data = await resp.json()
+      update(data.balance)
       return data
     }
   })
@@ -28,7 +37,7 @@ export const Balance = () => {
       <Center h={10}>
         <HStack spacing={3}>
           <Coins color={iconRgb} size="1.2rem" weight="regular" />
-          <Text color={iconColor}>{data.balance?.toLocaleString()}</Text>
+          <Text ref={countUpRef} color={iconColor}>{data.balance?.toLocaleString()}</Text>
         </HStack>
       </Center>
     </Box>
